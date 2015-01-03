68011746cdf7b3cded222d237ddfb1f17c6320bb
Rakesh Das <rakeshdaskowshik@gmail.com>
2015-01-03 01:10:50 -0600

    RSS URL

--- app/src/main/java/com/rakeshdas/rssparse/Downloader.java
+++ app/src/main/java/com/rakeshdas/rssparse/Downloader.java
@@ -1,11 +1,30 @@
 package com.rakeshdas.rssparse;
 
+import android.app.Activity;
+
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import nl.matshofman.saxrssreader.RssFeed;
 import nl.matshofman.saxrssreader.RssReader;
 
-public class Downloader {
-    URL url = new URL("http://reddit.com/r/FlashTV/.rss");
-    RssFeed feed = RssReader.read(url);
+public class Downloader extends Activity{
+    public void getRss(){
+        try{
+            URL redditURL = new URL("http://reddt.com/r/flashtv/.rss");
+            RssFeed feed = RssReader.read(redditURL);
+        }
+        catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch (SAXException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
