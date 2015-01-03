56571756a71eb11737df2fe51d0103680d2f84d4
Rakesh Das <rakeshdaskowshik@gmail.com>
2015-01-03 00:49:04 -0600

    Imported lib

--- app/src/main/java/com/rakeshdas/rssparse/menu1_Fragment.java
+++ app/src/main/java/com/rakeshdas/rssparse/menu1_Fragment.java
@@ -9,7 +9,6 @@ import android.view.ViewGroup;
 
 public class menu1_Fragment extends android.support.v4.app.Fragment{
     private String finalURL = "https://www.reddit.com/r/FlashTV/.rss";
-    private HandleXML obj;
     View rootview;
     @Nullable
     @Override
